ef50396949ec51b5d32a440a195bbf196fd3d420
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-02-13 21:51:40 +0400

    homework_lesson2

---
+++ src/Lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,75 @@
+package Lesson2;
+
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        byte b1 = 12;
+        byte b2 = 014;
+        byte b3 = 0b1100;
+        byte b4 = 0Xc;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 1300;
+        short s2 = 0X514;
+        short s3 = 02424;
+        short s4 = 0b10100010100;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0;
+        int i2 = 0b0;
+        int i3 = 0X0;
+        int i4 = 0;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 123456789L;
+        long l2 = 0X75BCD15L;
+        long l3 = 0726746425L;
+        long l4 = 0b111010110111100110100010101L;
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        float f1 = 1.5F;
+        float f2 = 5.8F;
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 0.1234d;
+        double d2 = 5.21245487d;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+        char ch1 = 'z';
+        char ch2 = '\u0555';
+        char ch3 = 77;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+
+    }
+
+}
