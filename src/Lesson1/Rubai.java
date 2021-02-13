52788863c6729edff7b5283a51a40eaaa0960edb
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-02-13 19:54:28 +0400

    homework_lesson1

---
+++ src/Lesson1/Rubai.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Rubai {
+
+    public static void main(String[] args) {
+
+        System.out.println("             РУБАИ           ");
+        System.out.println("hsbdhsfhhsdfhdbhfdfgjsgdfjgsdjf");
+        System.out.println("hsdfjdsfhdhsfhdsfghgfjsdgfh");
+        System.out.println("smhbfhsdfhdshfhdbhfdfjgdjfgdjs");
+        System.out.println("                  О. Хайям");
+
+    }
+
+}
