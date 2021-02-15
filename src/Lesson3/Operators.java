5d3cad7ab0d58855debd943d639b422cc6f99be6
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-02-15 21:00:21 +0400

    homework_lesson3

--- src/Lesson3/Operators.java
+++ src/Lesson3/Operators.java
@@ -6,9 +6,8 @@ public class Operators {
         int i2 = 11;
         double d1 = 5.5;
         double d2 = 1.3;
-        long l = 20l;
-        double result = 0;
-        result = i2 / d1 + d2 % i1 - l;
+        long l = 20L;
+        double result = i2 / d1 + d2 % i1 - l;
         System.out.println(result);
     }
 
