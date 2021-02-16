bac7414c67b499a10a378eb35775947c4dbd2f5b
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-02-16 20:24:37 +0400

    homework_lesson5

---
+++ src/Lesson5/BankAccount.java
@@ -0,0 +1,28 @@
+package Lesson5;
+
+public class BankAccount {
+    String name = "Petr";
+    double balance=100;
+
+    public void addBalance(double sum) {
+        System.out.println("Balance before refilling is " + balance);
+        balance += sum;
+        System.out.println("Balance after refilling is " + balance);
+        System.out.println();
+    }
+
+    public void minusBalance(double sum) {
+        System.out.println("Balance before withdrawing is " + balance);
+        balance -= sum;
+        System.out.println("Balance after withdrawing is " + balance);
+        System.out.println();
+    }
+}
+
+class BankAccountTest{
+    public static void main(String[] args) {
+        BankAccount MyAccount = new BankAccount();
+        MyAccount.addBalance(30.5);
+        MyAccount.minusBalance(10.5);
+    }
+}
