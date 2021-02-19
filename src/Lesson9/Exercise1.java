cf695f0e18f3cfc923a08d65880ef87c940219d5
arpharutyunyan <arpine.harutyunyana@gmail.com>
2021-02-19 19:59:08 +0400

    homework_lesson9

---
+++ src/Lesson9/Exercise1.java
@@ -0,0 +1,17 @@
+package Lesson9;
+
+
+public class Exercise1 {
+    static void method1() {
+        String st1 = "hdhdbvd";
+        String st2 = "sdjfdj";
+    }
+
+    public static void main(String[] args) {
+        Exercise1 ex = new Exercise1();
+        method1();
+        method1();
+        String st3 = "dsjkfjd";
+        method1();
+    }
+}
